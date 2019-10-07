44f4ba513de49e7f79cb595ef99616f2136d52ab
caoyou222 <caoyou12@gmail.com>
2019-10-07 13:59:36 -0400

    initialized the project and connected to backend

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount(){
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if(response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          {this.state.data}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
