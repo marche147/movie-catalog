cd151e78585acf05ecb2a7c49d88128f5d508b4a
caoyou222 <caoyou12@gmail.com>
2019-10-07 16:18:20 -0400

    connected to mongoDB Atlas

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+const mongoose = require('mongoose');
 const express = require('express');
+const API_PORT = 5000;
 const app = express();
-const port = process.env.PORT || 5000;
+const router = express.Router();
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// this is our MongoDB database
+const dbRoute =
+  'mongodb+srv://you_cao:22ohyeah@movie-catalog-9wta9.mongodb.net/admin?retryWrites=true&w=majority';
+
+// connects our back end code with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
