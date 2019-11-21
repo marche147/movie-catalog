f5cd545d9af39e67a72f49a8939cfff44ad8b905
Shaoqian Lu <shaoqianlu@yandex.com>
2019-11-20 22:07:42 -0500

    add many things

--- client/src/screens/list.js
+++ client/src/screens/list.js
@@ -16,17 +16,9 @@ const movies = [{ "id": 1, "name": "joker", "year": 2019, "director": "Joachim R
 { "id": 2, "name": "once upon a time in hollywood", "year": 2019, "director": "Quentin Tarantino", "cast": "Brad Pitt" },
 { "id": 3, "name": "avengers: endgame", "year": 2019, "director": "Chris Buck", "cast": "Idina Menzel" },
 { "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-{ "id": 4, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
-
-{ "id": 5, "name": "avengers1: endgame", "year": 20191, "director": "Chris Buck1", "cast": "Idina Menzel1" }
+{ "id": 5, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
+{ "id": 6, "name": "avengersd: endgame", "year": 20195, "director": "Chris Buckd", "cast": "Idina Menzeld" },
+{ "id": 14, "name": "avengers1: endgame", "year": 20191, "director": "Chris Buck1", "cast": "Idina Menzel1" }
 ]
 
 const { Option } = Select;
@@ -35,10 +27,20 @@ const { Option } = Select;
 //console.log('onSelect', value);
 //}
 
+
+/*
 function handleChange(value) {
   console.log(`selected ${value}`);
+  if(value=="rate"){
+    sortById();
+  }else if (value=="time"){
+    console.log('2');
+  }
 }
-
+function sortById(){
+  console.log('ok')
+}
+*/
 class List extends Component {
   getapi = (text) => {
     fetch(text)
@@ -75,43 +77,87 @@ class List extends Component {
     };
     this.getapi = this.getapi.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.clear = this.clear.bind(this);
+    this.handleChange=this.handleChange.bind(this);
   }
 
-  handleClick() {
-    let content = document.getElementById('search').value;
-    console.log(content)
-    //console.log('hi')
+  handleChange(value) {
+    console.log(value);
+    if("name"==value){
+      var m = this.state.items.title.sorter((a, b) => a.title > b.title);
+      this.setState({items:m});
+    }else if("time"==value){
+      var n = this.state.items.runtime.sorter((a, b) => a.runtime > b.runtime);
+      this.setState({items:n});
+    }
+  }
 
-    this.state = {
+
+
+  handleClick() {
+    //this.getapi("/list");
+    let content = document.getElementById('search').value;
+    //console.log(content)
+    console.log(`hi`)
+    this.match(content)
+    /*this.state = {
       error: null,
       isLoaded: false,
       items: []
     };
+    */
 
-    this.getapi("/list");
-    this.forceUpdate()
+    //this.getapi("/list");
+    //this.forceUpdate()
   }
 
+  clear() {
+    this.getapi("/list")
+  }
+
+  match(content) {
+    console.log(content)
+    console.log(this.state.items.length)
+    for (let i = 0; i < this.state.items.length; i++) {
+      if (content == this.state.items[i].title) {
+        console.log(`its ${this.state.items[i]}`)
+        console.log(this.state.items[i])
+
+        var m = [];
+        m.push(this.state.items[i]);
+        console.log(m);
+        this.setState({ items: m });
+      }
+    }
+
+    //let m=this.state.items[1]
+    //console.log(this.state.items[1].title)
+    //jokerthis.forceUpdate()
+    //console.log(`a new ${m}`)
+
+  }
   componentDidMount() {
+    //let m=this.state.items;
+    //console.log(`initialize ${m}`);
     this.getapi("/list");
+
+    //let m=this.state.items;
   }
 
   onSearch = searchText => {
     let content = document.getElementById('search').value;
-    console.log(content)
+    //console.log(content)
     //this.setState({
     //items: !searchText ? [] : [searchText, searchText.repeat(2), searchText.repeat(3)],
 
     //});
   };
 
-
+  //let m=this.state.items;
   render() {
     const { error, isLoaded, items } = this.state;
     //let itemsmap = items.map((m) => <div class="display"><li key={m._id}> <Link to={`/info/${m.id}`}> {m.title} </Link></li></div>)
     const { Meta } = Card;
-
-
 
     const menu = (
       <Menu>
@@ -128,7 +174,7 @@ class List extends Component {
       </Menu>
     );
 
-
+    //let m=this.state.items;
     //const listItems = movies.map((m) => <li key={m.id}> <Link to={`/info/${m.name}`}> {m.name} </Link></li>)
     let listItems = this.state.items.map((m) =>
       <div class="display">
@@ -140,7 +186,7 @@ class List extends Component {
               style={{ width: 200 }}
               cover={<img alt={m.title} src={m.img_url} />}
             >
-              <Meta title={m.title} description={m.release_date} />
+              <Meta title={m.title} description={m.cast} />
               <li key={m._id}> <Link to={`/info/${m._id}`}> info </Link></li>
             </Card>
           </Col>
@@ -153,7 +199,7 @@ class List extends Component {
     if (error) {
       return (<div className="container">
         <div className="search">
-          <Select defaultValue="name" style={{ width: 120 }} onChange={handleChange}>
+          <Select defaultValue="name" style={{ width: 90 }} onChange={this.handleChange}>
             <Option value="name">name</Option>
             <Option value="year">year</Option>
             <Option value="cast">cast</Option>
@@ -168,6 +214,15 @@ class List extends Component {
               Top Movies <Icon type="down" />
             </a>
           </Dropdown>
+          <br />
+          <Row type="flex" justify="end">
+            <Col span={4}>
+              <Select defaultValue="Sort by: Featured" id="choice" style={{ width: 170 }} onChange={this.handleChange}>
+                <Option value="name">Alphabetical order</Option>
+                <Option value="time">Runtime</Option>
+              </Select>
+            </Col>
+          </Row>
         </div>
         <div>
           <Row>
@@ -196,7 +251,7 @@ class List extends Component {
       return (
         <div className="container">
           <div className="search">
-            <Select defaultValue="name" style={{ width: 120 }} onChange={handleChange}>
+            <Select defaultValue="name" style={{ width: 90 }} onChange={this.handleChange}>
               <Option value="name">name</Option>
               <Option value="year">year</Option>
               <Option value="cast">cast</Option>
@@ -206,24 +261,34 @@ class List extends Component {
               id="search"
               style={{ width: 200 }} />
             <button onClick={this.handleClick}>search</button>
+            <button onClick={this.clear}>clear</button>
             <Dropdown overlay={menu}>
               <a className="dropdown-link" href="#">
                 Top Movies <Icon type="down" />
               </a>
             </Dropdown>
+            <br />
+            <Row type="flex" justify="end">
+              <Col span={4}>
+                <Select defaultValue="Sort by: Featured" style={{ width: 170 }} onChange={this.handleChange}>
+                <Option value="name">Alphabetical order</Option>
+                <Option value="time">Runtime</Option>
+                </Select>
+              </Col>
+            </Row>
           </div>
 
           <div className="home">
-          <Row>
-            {listItems}
-          </Row>
+            <Row>
+              {listItems}
+            </Row>
             <Route path="/info/:id" component={Info} />
           </div>
           <div>
-          <BackTop />
-          Scroll down to see the bottom-right
+            <BackTop />
+            Scroll down to see the bottom-right
            <strong style={{ color: 'rgba(64, 64, 64, 0.6)' }}> gray </strong>
-          button.
+            button.
          </div>
         </div>
       );
