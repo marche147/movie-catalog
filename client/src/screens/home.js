49dd8fb5b5a86e82dbaceef6d3a438b14042cd8b
marche147 <i@shiki7.me>
2019-12-07 12:06:21 -0500

    Change home.js

--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -1,281 +1,138 @@
-import logo from '../logo.svg';
-import { Input } from 'antd';
-import { Layout, Menu, Breadcrumb } from 'antd';
 import React, { Component } from 'react';
-import {  Dropdown, Icon } from 'antd';
-import { Button } from 'antd';
-import { Carousel } from 'antd';
-import { Card, Col, Row } from 'antd';
-import "./styles.css";
-import { List, Typography } from 'antd';
-
+import { Layout, Menu, Carousel, Card, Col, Row, List } from 'antd';
 const { Header, Content, Footer } = Layout;
-const { Meta } = Card;
+const { Meta } = Card
+import "./styles.css";
+import { sortBy, take, reverse } from 'lodash'
+import md5 from 'crypto-js/md5';
 
-class Home extends Component{
-    getapi = (text) => {
-    fetch(text)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result);
-          this.setState({
-            isLoaded: true,
-            items: result
+/* Capitalize title */
+let capitalize = (title) => {
+  let words = title.split(" ");
+  let res = words.map((word) => { return (word[0] && word[0].toUpperCase()) + word.substr(1); });
+  return res.join(" ");
+};
 
-          });
-
-        },
-        (error) => {
-          console.log(error)
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
+class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
+      data: [],
+      movies: [],
     };
-    this.getapi = this.getapi.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange=this.handleChange.bind(this);
-  }
-
-  handleChange(value) {
-    console.log(value);
-    if("name"==value){
-      var m = this.state.items.title.sorter((a, b) => a.title > b.title);
-      this.setState({items:m});
-    }else if("time"==value){
-      var n = this.state.items.runtime.sorter((a, b) => a.runtime > b.runtime);
-      this.setState({items:n});
-    }
-  }
-
-  handleClick() {
-    let content = document.getElementById('search').value;
-    console.log(`hi`)
-    this.match(content)
-  }
-
-    match(content) {
-    console.log(content)
-    console.log(this.state.items.length)
-    for (let i = 0; i < this.state.items.length; i++) {
-      if (content == this.state.items[i].title) {
-        console.log(`its ${this.state.items[i]}`)
-        console.log(this.state.items[i])
-
-        var m = [];
-        m.push(this.state.items[i]);
-        console.log(m);
-        this.setState({ items: m });
-      }
-    }
   }
 
   componentDidMount() {
-    this.getapi("/movies");
+    this.retrieve().then(res => { this.setState({ data: res }) }).catch(err => { console.log(err) });
+    this.fetchMovies().then(res => { this.setState({ movies: res }) }).catch(err => { console.log(err) });
   }
 
-  onSearch = searchText => {
-    let content = document.getElementById('search').value;
-  };
+  retrieve = async () => {
+    const resp = await fetch("/top");
+    if(resp.status !== 200) {
+      return [];
+    }
+    const body = resp.json();
+    return body;
+  }
 
-  onChange = value => {
-    this.setState({ value });
-  };
+  fetchMovies = async () => {
+    const resp = await fetch("/movies");
+    if(resp.status !== 200) {
+      return [];
+    }
+    const body = resp.json();
+    return body;
+  }
+
 
   render() {
-    
-    const { error, isLoaded, items } = this.state;
+    // console.log(this.state.movies,999)
+    const arr = this.state.movies.filter(o => o.img_url), result = [];    
+    for (let i = 0; i < 6; i++) {    
+      const pos = Math.floor(Math.random() * (arr.length - i));      
+      result.push(arr[pos]);      
+      arr[pos] = arr[arr.length - i - 1];    
+    };
+
+    const sortMovies = key => sortBy(this.state.data, item => {
+      if (item[key] && item[key].indexOf('%') > -1) {
+        return +(item[key].replace(/[^0-9.]/ig, ''))
+      }
+      return +item[key] || 0
+    }).filter(item => item[key]) //filter the empty values
+
+    const getMoviesInAscOrder = key => take((sortMovies(key)), 10)
+    const getMoviesInDescOrder = key => take(reverse(sortMovies(key)), 10)
+
+    const rankList = [
+      { title: 'Rotten Tomatoes top Ranking', list: getMoviesInAscOrder('rt_rank') },
+      { title: 'IMDB top Ranking', list: getMoviesInAscOrder('imdb_rank') },
+      { title: 'Rotten Tomatoes top Rating', list: getMoviesInDescOrder('rt_rate') },
+      { title: 'IMDB top Rating', list: getMoviesInDescOrder('imdb_rate') },
+    ]
+
     return (
       <Layout className="layout">
-      <Header>
-              <div className="logo" />
-              <Menu
-                theme="dark"
-               mode="horizontal"
-               defaultSelectedKeys={['2']}
-                style={{ lineHeight: '64px' }}
-            >
-              <Menu.Item key="1">
-                RT
-              </Menu.Item>
-              <Menu.Item key="2">IMDB</Menu.Item>
-              <Menu.Item key="3">Favorite</Menu.Item>       
-            </Menu>
-          </Header>
+        <Header>
+          <div className="logo" />
+ 
+        </Header>
 
-    <Content style={{ padding: '0 50px' }}>
+        <Content style={{ padding: '0 50px' }}>
+          <center>
+            <div style={{ background: '#fff', width: 960, padding: 24, minHeight: 280 }}>
+              <Carousel autoplay>
+                <div> <img src="/pic/1.jpeg" /> </div>
+                <div> <img src="/pic/2.jpeg" /> </div>
+                <div> <img src="/pic/3.jpeg" /> </div>
+                <div> <img src="/pic/4.jpeg" /> </div>
+              </Carousel>
+            </div>
+          </center>
 
-      <center><div style={{ background: '#fff', width: 960, padding: 24, minHeight: 280 }}>
-      <Carousel autoplay>
-        <div>
-          <img src ="/pic/1.jpeg" />
-        </div>
-        <div>
-          <img  src="/pic/2.jpeg" />
-        </div>
-        <div>
-          <img  src="/pic/3.jpeg" />
-        </div>
-        <div>
-          <img  src="/pic/4.jpeg" />
-        </div>
-        </Carousel>
-        </div></center>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-          <div style={{ background: '#ECECEC', padding: '30px' }}>
-          <Row gutter={16}>
-            <Col span={6}>
-              <Card title="Rotten Tomatoes top10 Ranking" bordered={false}>
-                Avengers:Endgame
-                <br/>
-                <br/>
-                Us
-                <br/>
-                <br/>
-                Toy Story 4
-                <br/>
-                <br/>
-                The Farewell
-                <br/>
-                <br/>
-                Booksmart
-                <br/>
-                <br/>
-                Parasite
-                <br/>
-                <br/>
-                If Beale Street Could Talk
-                <br/>
-                <br/>
-                Spider-man: Far From Home
-                <br/>
-                <br/>
-                Once Upon A Time In Hollywood
-                <br/>
-                <br/>
-                Shazam!
-                <br/>
-              </Card>
-            </Col>
-            <Col span={6}>
-              <Card title="IMDB top10 Ranking" bordered={false}>
-                Joker
-                <br/>
-                <br/>
-                Parasite
-                <br/>
-                <br/>
-                Avengers:Endgame
-                <br/>
-                <br/>
-                Spider-man: Into The Spider-verse
-                <br/>
-                <br/>
-                Capernaum
-                <br/>
-                <br/>
-                Green Book
-                <br/>
-                <br/>
-                Bacurau
-                <br/>
-                <br/>
-                The Biggest Little Farm
-                <br/>
-                <br/>
-                Once Upon A Time In Hollywood
-                <br/>
-                <br/>
-                Toy Story 4
-                <br/>
-              </Card>
-            </Col>
-            <Col span={6}>
-              <Card title="Rotten Tomatoes top10 Rating" bordered={false}>
-                They Shall Not Grow Old
-                <br/>
-                <br/>
-                Knock Down The House
-                <br/>
-                <br/>
-                Chained For Life
-                <br/>
-                <br/>
-                For Sama
-                <br/>
-                <br/>
-                The Chambermaid
-                <br/>
-                <br/>
-                Amazing Grace
-                <br/>
-                <br/>
-                Bacurau
-                <br/>
-                <br/>
-                Apollo 11
-                <br/>
-                <br/>
-                Ash Is Purest White
-                <br/>
-                <br/>
-                The Farewell
-                <br/>
-              </Card>
-            </Col>
-            <Col span={6}>
-              <Card title="IMDB top10 Rating" bordered={false}>
-                Avengers: Endgame
-                <br/>
-                <br/>
-                Joker
-                <br/>
-                <br/>
-                Shoplifters
-                <br/>
-                <br/>
-                The Farewell
-                <br/>
-                <br/>
-                The Biggest Little Farm
-                <br/>
-                <br/>
-                Once Upon A Time In Hollywood
-                <br/>
-                <br/>
-                Toy Story 4
-                <br/>
-                <br/>
-                Bacurau
-                <br/>
-                <br/>
-                Green Book
-                <br/>
-                <br/>
-                Capernaum
-                <br/>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-    </div> 
-    </Content>
+          <div style={{ background: '#ECECEC', padding: '30px 0' }}>
+              <Row gutter={16}>{
+                result.map((movie, movieIndex) => {
+                  const url = movie ? movie.img_url : ''
+                  const title = movie ? movie.title : ' '
+                  return (
+                    <Col span={4}>
+                      <Card title={null} bordered={false} cover={<img src={url}/>}>
+                      <Meta title={ <a href={`/info/${md5(title)}`}>{capitalize(title)}</a>} />
+                      </Card>
+                    </Col>
+                  )
+                })
+              }
+              </Row>
+            </div>
+
+          <div style={{ background: '#ECECEC', padding: '30px 0' }}>
+              <Row gutter={16}>{
+                rankList.map((rank, rankIndex) => {
+                  return (
+                    <Col span={6} key={rankIndex}>
+                      <Card title={rank.title} bordered={false}>
+                        <List
+                          size="large"
+                          dataSource={rank.list}
+                          renderItem={(item, index) =>
+                            <List.Item>
+                              <a href={`/info/${md5(item.title)}`}>{index + 1}. {capitalize(item.title)}</a>
+                            </List.Item>}
+                        />
+                      </Card>
+                    </Col>
+                  )
+                })
+              }
+              </Row>
+            </div>
+        </Content>
         <Footer style={{ textAlign: 'center' }}>Group 1 design @ 2019</Footer>
-          </Layout>
-      );
+      </Layout>
+    );
   };
 }
 export default Home
-  
-    
